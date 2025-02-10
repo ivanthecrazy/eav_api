5a1673143955202f0a7c69fef529319c8b0bacc6
Ivan Larionov <ivan.v.larionov@proton.me>
2025-02-10 16:18:58 +0400

    fix attributes seeder

--- database/seeders/AttributeSeeder.php
+++ database/seeders/AttributeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Attribute;
+use App\Enums\AttributeType;
 
 class AttributeSeeder extends Seeder
 {
@@ -12,8 +13,8 @@ class AttributeSeeder extends Seeder
      */
     public function run(): void
     {
-        Attribute::create(['name' => 'department', 'type' => 'text']);
-        Attribute::create(['name' => 'start_date', 'type' => 'date']);
-        Attribute::create(['name' => 'end_date', 'type' => 'date']);
+        Attribute::create(['name' => 'department', 'type' => AttributeType::TEXT]);
+        Attribute::create(['name' => 'start_date', 'type' => AttributeType::DATE]);
+        Attribute::create(['name' => 'end_date', 'type' => AttributeType::DATE]);
     }
 }
